fbb99c141c76ac4d303a6c384a0c070028a9c492
vandeurenglenn <vandeurenglenn@gmail.com>
2020-01-13 01:55:38 +0100

    add wss

--- commonjs.js
+++ commonjs.js
@@ -154,13 +154,13 @@ class LeofcoinApi extends DiscoBus {
             '/ip4/45.137.149.26/tcp/4430/ws/p2p-websocket-star'
           ]
         },
-        Bootstrap: [          
+        Bootstrap: [
+          '/ip4/45.137.149.26/tcp/4003/wss/ipfs/QmURywHMRjdyJsSXkAQyYNN5Z2JoTDTPPeRq3HHofUKuJ4',
+          '/p2p-circuit/ip4/45.137.149.26/tcp/4003/wss/ipfs/QmURywHMRjdyJsSXkAQyYNN5Z2JoTDTPPeRq3HHofUKuJ4',
           '/ip4/45.137.149.26/tcp/4003/ws/ipfs/QmURywHMRjdyJsSXkAQyYNN5Z2JoTDTPPeRq3HHofUKuJ4',
           '/p2p-circuit/ip4/45.137.149.26/tcp/4003/ws/ipfs/QmURywHMRjdyJsSXkAQyYNN5Z2JoTDTPPeRq3HHofUKuJ4',
           '/ip4/45.137.149.26/tcp/4002/ipfs/QmURywHMRjdyJsSXkAQyYNN5Z2JoTDTPPeRq3HHofUKuJ4',
-          '/p2p-circuit/ip4/45.137.149.26/tcp/4002/ipfs/QmURywHMRjdyJsSXkAQyYNN5Z2JoTDTPPeRq3HHofUKuJ4',
-          '/ip4/45.137.149.26/tcp/4001/ipfs/QmP4tp5VoUNmGzApvFvQPkMXHLzafr7ULmhiswpeJ2fd4Z', 
-          '/p2p-circuit/ip4/45.137.149.26/tcp/4001/ipfs/QmP4tp5VoUNmGzApvFvQPkMXHLzafr7ULmhiswpeJ2fd4Z'          
+          '/p2p-circuit/ip4/45.137.149.26/tcp/4002/ipfs/QmURywHMRjdyJsSXkAQyYNN5Z2JoTDTPPeRq3HHofUKuJ4'    
         ]
       },
       EXPERIMENTAL: { ipnsPubsub: true, sharding: true }
