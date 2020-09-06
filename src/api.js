3d84bcd9c81768faf14a6b68bb48b45e5db53a9a
vandeurenglenn <vandeurenglenn@gmail.com>
2020-09-06 17:25:46 +0200

    Connect star to earth

--- src/api.js
+++ src/api.js
@@ -16,7 +16,7 @@ const https = (() => {
 })();
 
 export default class LeofcoinApi extends DiscoBus {
-  constructor(options = { init: true, start: true, bootstrap: 'lfc', forceJS: false, star: false, network: 'leofcoin' }) {
+  constructor(options = { init: true, start: true, forceJS: false, star: false, network: 'leofcoin' }) {
     super()
     this.network = options.network || 'leofcoin'
 
@@ -74,6 +74,7 @@ export default class LeofcoinApi extends DiscoBus {
       globalThis.walletStore = new LeofcoinStorage('lfc-wallet', `.leofcoin/${this.network}`)
       const account = await accountStore.get()
       wallet = await walletStore.get()
+      
       if (!wallet.identity) {
         const { identity, accounts, config } = await this.account.generateProfile()
         wallet.identity = identity
@@ -91,8 +92,7 @@ export default class LeofcoinApi extends DiscoBus {
         }
         
         // ensure we don't need barbaric methods again.
-        if (!wallet.version) wallet.version = 1
-        
+        if (!wallet.version) walletStore.put('version', 1)
         // TODO: convert accounts[account] to objbased { name, addresses }
       }
       const multiWallet = new MultiWallet(this.network)
@@ -106,17 +106,17 @@ export default class LeofcoinApi extends DiscoBus {
       // const client = new HttpClient({ host: '127.0.0.1', port: 5050, pass: wallet.identity.privateKey});
       // globalThis.ipfs = client.ipfs
       // globalThis.api = client.api
-      return await this.spawnJsNode(wallet, config.bootstrap, config.star, config.target)
+      return await this.spawnJsNode(wallet, config.star, config.target)
     }
   }
 
-  async _init({start, bootstrap, forceJS, star}) {
-    await this._spawn({bootstrap, star}, forceJS)
+  async _init({start, forceJS, star}) {
+    await this._spawn({ star}, forceJS)
     // await globalApi(this)
     return this
   }
 
-  async spawnJsNode (config, bootstrap, star, { environment }) {
+  async spawnJsNode (config, star, { environment }) {
     await IPFS_IMPORT
       if (environment === 'node') {
         config.Addresses = {
@@ -141,23 +141,23 @@ export default class LeofcoinApi extends DiscoBus {
         }
       }
 
-    if (bootstrap === 'lfc') {
-      if (environment === 'browser' && https || environment === 'electron') {
-        bootstrap = [
-         '/dns4/star.leofcoin.org/tcp/4003/wss/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4'
-       ]
-     } else if (environment === 'node') {
-        bootstrap = [
-         '/dns4/star.leofcoin.org/tcp/4020/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4',
-         '/dns4/star.leofcoin.org/tcp/4003/wss/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4'
-       ];
-     } else {
-       bootstrap = [
-        '/dns4/star.leofcoin.org/tcp/4003/wss/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4'
-      ];
-     }
-
-    } else if (star) bootstrap = [];
+    // if (bootstrap === 'lfc') {
+    //   if (environment === 'browser' && https || environment === 'electron') {
+    //     bootstrap = [
+    //      '/dns4/star.leofcoin.org/tcp/4003/wss/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4'
+    //    ]
+    //  } else if (environment === 'node') {
+    //     bootstrap = [
+    //      '/dns4/star.leofcoin.org/tcp/4020/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4',
+    //      '/dns4/star.leofcoin.org/tcp/4003/wss/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4'
+    //    ];
+    //  } else {
+    //    bootstrap = [
+    //     '/dns4/star.leofcoin.org/tcp/4003/wss/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4'
+    //   ];
+    //  }
+    // 
+    // } else if (star) bootstrap = [];
 
     config = {
       pass: config.identity.privateKey,
@@ -225,27 +225,27 @@ export default class LeofcoinApi extends DiscoBus {
       },
       config: {
         
-        API: {
-          "HTTPHeaders": {
-            "Access-Control-Allow-Origin": [
-              "http://localhost:5001",
-              "*"
-            ],
-            "Access-Control-Allow-Methods": [
-              "GET",
-              "PUT",
-              "POST"
-            ]
-          }
-        },
-        Bootstrap: bootstrap,
+        // API: {
+        //   "HTTPHeaders": {
+        //     "Access-Control-Allow-Origin": [
+        //       "http://localhost:5001",
+        //       "*"
+        //     ],
+        //     "Access-Control-Allow-Methods": [
+        //       "GET",
+        //       "PUT",
+        //       "POST"
+        //     ]
+        //   }
+        // },
+        // Bootstrap: bootstrap,
         Discovery: {
           MDNS: {
-            Enabled: Boolean(!globalThis.navigator || /electron/i.test(navigator.userAgent)),
-            Interval: 1000
+            enabled: Boolean(!globalThis.navigator || /electron/i.test(navigator.userAgent)),
+            interval: 1000
           },
           webRTCStar: {
-            Enabled: true
+            enabled: true
           },
           websocketStar: {
             enabled: true
@@ -271,25 +271,62 @@ export default class LeofcoinApi extends DiscoBus {
 
     try {
       globalThis.ipfs = await Ipfs.create(config)
+      const version = await walletStore.get('version')
+      if (version !== 2 && star) {
+        const earth = [
+          '/dns4/ams-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd',
+          '/dns4/lon-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3',
+          '/dns4/sfo-3.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM',
+          '/dns4/sgp-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu',
+          '/dns4/nyc-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm',
+          '/dns4/nyc-2.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64',
+          '/dns4/node0.preload.ipfs.io/tcp/443/wss/p2p/QmZMxNdpMkewiVZLMRxaNxUeZpDUb34pWjZ1kZvsd16Zic',
+          '/dns4/node1.preload.ipfs.io/tcp/443/wss/p2p/Qmbut9Ywz9YEDrz8ySBSgWyJk41Uvm2QJPhwDJzJyGFsD6',
+          '/dns4/node2.preload.ipfs.io/tcp/443/wss/p2p/QmV7gnbW5VTcJ3oyM2Xk1rdFBJ3kTkvxc87UFGsun29STS',
+          '/dns4/node3.preload.ipfs.io/tcp/443/wss/p2p/QmY7JB6MQXhxHvq7dBDh4HpbH29v4yE9JRadAVpndvzySN',
+          '/dns4/star.leofcoin.org/tcp/4003/wss/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4'
+        ]
+        const strap = await ipfs.config.get('Bootstrap')
+        await ipfs.config.set('Bootstrap', earth)
+        
+        walletStore.put('version', 2)
+      }
+      
       const { id, addresses, publicKey } = await ipfs.id()
       this.addresses = addresses
       this.peerId = id
       this.publicKey = publicKey
 
-      const strap = await ipfs.config.get('Bootstrap')
-      for (const addr of strap) {
-        await ipfs.swarm.connect(addr)
+      if (!star) {
+        const strap = await ipfs.config.get('Bootstrap')
+        
+        const index = strap.indexOf('/dns4/star.leofcoin.org/tcp/4003/wss/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4')
+        if (index === -1) {
+          strap.push('/dns4/star.leofcoin.org/tcp/4003/wss/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4')
+          await ipfs.config.set('Bootstrap', strap)
+          await ipfs.swarm.connect('/dns4/star.leofcoin.org/tcp/4003/wss/p2p/QmfShD2oP9b51eGPCNPHH3XC8K28VLXtgcR7fhpqJxNzH4')  
+        }
+        
+        for (const addr of strap) {
+          await ipfs.swarm.connect(addr)
+        }
       }
+      
       
       GLOBSOURCE_IMPORT
       
       ipfs.addFromFs = async (path, recursive = false) => {
         const files = []
-        for await (const file of globalThis.ipfs.addAll(globSource(path, { recursive }))) {
-          files.push(file)
+        try {
+          for await (const file of globalThis.ipfs.addAll(globSource(path, { recursive }))) {
+            files.push(file)
+          }
+        } catch (e) {
+          throw e
         }
         return files;
       }
+      
     } catch (e) {
       console.error(e);
     }
